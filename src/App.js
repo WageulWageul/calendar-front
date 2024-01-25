b4d83bea3065497f9a6213cb00d69f37bf8e8970
jiminseong <iamjms4237@gmail.com>
2024-01-25 14:59:14 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Home from './comp/home/Home';
+import Login from './comp/login/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '././index.css';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+       <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
